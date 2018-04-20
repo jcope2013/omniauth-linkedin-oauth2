d3c49cdb029c182c72ca909eb81aee21e64563d2
Mike Desjardins <desjardinsmike@gmail.com>
2018-04-20 18:12:34 -0400

    Adds preliminary rough support for LinkedIn v2 API.

--- lib/omniauth/strategies/linkedin.rb
+++ lib/omniauth/strategies/linkedin.rb
@@ -16,6 +16,7 @@ module OmniAuth
 
       option :scope, 'r_basicprofile r_emailaddress'
       option :fields, ['id', 'email-address', 'first-name', 'last-name', 'headline', 'location', 'industry', 'picture-url', 'public-profile-url']
+      option :api_version, 'v1'
 
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
@@ -25,6 +26,7 @@ module OmniAuth
       uid { raw_info['id'] }
 
       info do
+        if options.api_version == "v1"
         {
           :name => user_name,
           :email => raw_info['emailAddress'],
@@ -38,6 +40,23 @@ module OmniAuth
             'public_profile' => raw_info['publicProfileUrl']
           }
         }
+        elsif options.api_version == "v2"
+        {
+          :name => user_name,
+          :email => '',
+          :nickname => user_name,
+          :first_name => raw_info['localizedFirstName'],
+          :last_name => raw_info['localizedLastName'],
+          :location => "", # LIv2 TODO
+          :description => "",
+          :image => "",  # LIv2 TODO - this is going to be nasty to fetch.
+          :urls => {
+            'public_profile' => "" # LIv2 TODO
+          }
+        }
+        else
+          {}
+        end
       end
 
       extra do
@@ -60,7 +79,16 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get("/v1/people/~:(#{option_fields.join(',')})?format=json").parsed
+        if options.api_version == "v1"
+          @raw_info ||= access_token.get("/v1/people/~:(#{option_fields.join(',')})?format=json").parsed
+        elsif options.api_version == "v2"
+          # LIv2 TODO should use a projection to limit what we're pulling back but we'll need two
+          # different option_fields?
+          # @raw_info ||= access_token.get("/v2/me?projection=(#{option_fields.join(',')})").parsed
+          @raw_info ||= access_token.get("/v2/me").parsed
+        else
+          raise ArgumentError.new("Unexpected value for api_version option: #{options.api_version}")
+        end
       end
 
       private
@@ -72,7 +100,12 @@ module OmniAuth
       end
 
       def user_name
-        name = "#{raw_info['firstName']} #{raw_info['lastName']}".strip
+        # LIv2 Gross.
+        name = case options.api_version
+               when "v1" then "#{raw_info['firstName']} #{raw_info['lastName']}".strip
+               when "v2" then "#{raw_info['localizedFirstName']} #{raw_info['localizedLastName']}".strip
+               else ""
+               end
         name.empty? ? nil : name
       end
     end
